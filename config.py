9379cc65817296e1c6c042199d5f947699ea9ae2
JGG7 <julianandres.jgg@gmail.com>
2026-05-12 10:08:36 -0500

    Favor  trabajar en el puerto 3307 para SQL

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ class config:
     MYSQL_HOST = os.getenv('MYSQL_HOST')
     MYSQL_USER = os.getenv('MYSQL_USER')
     MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
-    MYSQL_PORT = int(os.getenv('MYSQL_PORT', 3306))
+    MYSQL_PORT = int(os.getenv('MYSQL_PORT', 3307))
     MYSQL_DB = os.getenv('MYSQL_DB')
     SECRET_KEY = os.getenv('SECRET_KEY')
